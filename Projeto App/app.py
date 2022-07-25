c044a41a5037a1019a6e7d63ea664bb72217395f
Guilherme Suusa da Cruz <gui.sdacruz6@users.noreply.github.com>
2022-07-25 14:02:21 -0300

    .

--- Projeto App/app.py
+++ Projeto App/app.py
@@ -10,5 +10,8 @@ GUI = Builder.load_file('tela.kv')
 class Comanda(App):
     def build(self):
         return GUI
+    def on_start(self):
+        print(self.root.ids)
+        self.root.ids['t1'].text = 'Editei o 1'
 
 Comanda().run()
