63113d78dfe0ebb37dd8f46becd27bf73190cf40
Guilherme Suusa da Cruz <gui.sdacruz6@users.noreply.github.com>
2022-09-06 15:02:35 -0300

    Create tutorial.py

---
+++ Tkinter_Aulas/tutorial.py
@@ -0,0 +1,16 @@
+#importação da biblioteca
+from tkinter import *
+
+#Variavel para criar a tela
+app=Tk()
+
+#Define o nome da janela
+app.title('Tutorial de Interface gráfica')
+
+#Define o tamanho da janela
+app.geometry('500x300')
+
+#Define as configurações da janela
+app.configure(background='#5F2599')
+
+app.mainloop()
