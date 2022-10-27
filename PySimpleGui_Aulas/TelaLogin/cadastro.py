4a1103db5408d8f1971fd5aa8ceebafd850d4301
Guilherme Suusa da Cruz <gui.sdacruz6@users.noreply.github.com>
2022-10-27 15:22:02 -0300

    .

---
+++ PySimpleGui_Aulas/TelaLogin/cadastro.py
@@ -0,0 +1,20 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+#Layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('User:'),sg.Input(key='user')],
+    [sg.Text('Senha:',sg.Input(key='senha',password_char='*'))],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')],
+    
+]
+#Janela
+janela = sg.Window('Tela de login', layout)
+#ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'jhonatan'
