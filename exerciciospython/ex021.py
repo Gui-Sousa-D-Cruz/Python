5a9c87c8023d6cc24e5fffb340c40a26e18c9b3e
Guilherme Suusa da Cruz <gui.sdacruz6@users.noreply.github.com>
2023-03-28 21:21:59 -0300

    .

--- exerciciospython/ex021.py
+++ exerciciospython/ex021.py
@@ -1,19 +1,35 @@
 
 import pygame
 
-# Inicializando o mixer PyGame
-pygame.mixer.init()
+def music():
+    # Inicializando o mixer PyGame
+    pygame.mixer.init()
 
-# Iniciando o Pygame
-pygame.init()
+    # Iniciando o Pygame
+    pygame.init()
 
-#Carregando a música
-pygame.mixer.music.load('exerciciospython/ex021a.mp3')
+    #Carregando a música
+    pygame.mixer.music.load('exerciciospython/ex021a.mp3')
 
-#Configurando como a música vai ser executada
-pygame.mixer.music.play(loops=0, start=56.0)
+    #Configurando como a música vai ser executada
+    pygame.mixer.music.play(loops=0, start=56.0)
 
-#Espera a música acabar pra fechar o programa
-pygame.event.wait()
-
-
+    #Espera a música acabar pra fechar o programa
+    pygame.event.wait()
+print('')
+print('')
+esc = input('Você está pronta? Sim ou não? ')
+print('')
+print('')
+if esc.upper() == 'SIM':
+    print('Oi Duda, mesmo eu tendo um jeito meio estranho e estando meio mal, eu te amo muito')
+    print('')
+    print('')
+    esc = input('Você me ama? Sim ou não? ')
+    print('')
+    print('')
+    if esc.upper() == 'SIM':
+        print('OBRIGADO POR TUDO!')
+        music()
+else:
+    print('Vai se fuder então!')
